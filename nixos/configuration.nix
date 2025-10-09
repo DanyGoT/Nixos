2c3a4a99f1d1c3475e7373a6d06f5bac38ec13d4
DanyGoT <danygonzteig@gmail.com>
2025-10-09 19:30:35 +0200

    init

---
+++ nixos/configuration.nix
@@ -0,0 +1,167 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings.General.Experimental = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dany = {
+    isNormalUser = true;
+    description = "Dany";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "dany";
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    neovim
+    gcc
+    gnumake
+    lua51Packages.luarocks
+    lua51Packages.lua
+    lua-language-server
+    elixir
+    csharp-ls
+    dotnet-sdk_9
+    dotnet-ef
+    go
+    gopls
+    typescript-language-server
+    python3Full
+    python313Packages.python-lsp-server
+
+
+
+
+    fzf
+    zoxide
+    ripgrep
+    stow
+
+
+
+
+    vscode
+    chromium
+    google-chrome
+    discord
+    teams-for-linux
+    sway
+    ghostty
+    fuzzel
+    grim
+    slurp
+    mako
+    wl-clipboard
+    brightnessctl
+    
+    postgresql
+    gimp
+    libreoffice
+    wget
+  ];
+  fonts = {
+    fontconfig.enable = true;
+    packages = with pkgs; [
+      _0xproto
+    ];
+  };
+
+  programs.git = {
+    enable = true;
+  config = {
+      pull.rebase = true;
+    };
+  };
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+  programs.light.enable = true;
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "robbyrussell";
+    };
+  };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.sessionVariables.EDITOR = "nvim";
+  environment.sessionVariables.VISUAL = "nvim";
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.seatd.enable = true;
+  services.blueman.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "25.05"; # Did you read the comment?
+
+}
