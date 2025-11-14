fe6abdd9fd229a37fb9c2a8cf509e0b900363ed5
DanyGoT <danygonzteig@gmail.com>
2025-11-14 10:01:37 +0100

    nixos(nvim): made background transparent

--- nixos/nvim/init.lua
+++ nixos/nvim/init.lua
@@ -3,3 +3,7 @@ require('jonasvim')
 vim.opt.laststatus = 3 -- Required for proper window handling
 
 vim.cmd 'colorscheme vscode'
+vim.cmd [[
+  highlight Normal guibg=none ctermbg=none
+  highlight NonText guibg=none ctermbg=none
+]]
